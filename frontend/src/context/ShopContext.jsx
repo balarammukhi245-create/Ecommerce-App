b6b87dea5fe7c2be87f18799e103a830dd1857fa
balarammukhi245-create <balarammukhi245@gmail.com>
2026-03-21 17:07:53 +0530

    feat: add navigation to checkout in Cart component and update ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom"
 
 export const ShopContext = createContext();
 
@@ -9,7 +10,8 @@ const ShopContextProvider = (props)=>{
     const delivery_fee = 10;
     const [search, setSearch] = useState("");
     const [showSearch, setShowSearch] = useState(false);
-    const [cartItems, setCartItems] = useState({});
+    const [cartItems, setCartItems] = useState({}); 
+    const navigate = useNavigate();
 
     const addToCart = async(itemId, size)=>{
 
@@ -79,7 +81,8 @@ const ShopContextProvider = (props)=>{
         cartItems,addToCart, 
         getCartCount,
         updateQuantity,
-        getCartAmount
+        getCartAmount,
+        navigate
     }
 
     return (
