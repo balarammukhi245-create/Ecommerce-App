3e4a4439de29acf0f48b92dc025c9f0583cbdc58
balarammukhi245-create <balarammukhi245@gmail.com>
2026-03-30 08:30:17 +0530

    setup for Admin

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+  tailwindcss(),
   ],
-    server: {
-    port: 5173
+  server: {
+    port: 5174
   }
 })
--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import { ApiError }from "../utils/ApiError.js"
-import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 
