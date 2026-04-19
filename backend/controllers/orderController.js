be2585196e00f6eae230d88ce01b6cdc1d60d99d
balarammukhi245-create <balarammukhi245@gmail.com>
2026-04-19 20:24:52 +0530

    feat(order): implement Stripe payment integration for order placement with validation

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,6 +3,14 @@ import userModel from "../models/userModels.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
+import Stripe from "stripe"
+
+// global variables
+const cureency = "inr";
+const deliveryCharge = 10;
+
+// getway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 //Placing order using COD Method
@@ -37,6 +45,64 @@ const placeOrder = asyncHandler( async (req, res) => {
 );
 // Placing order using Stripe Payment Gateway
 const placeOrderStripe = asyncHandler( async (req, res) => {
+
+    const { userId, items, amount, address} = req.body;
+    const { origin } = req.headers;
+
+    if (!userId || !items || !amount || !address){
+        throw new ApiError(400, "All fields are required")
+     }
+
+   
+     
+     const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: "Stripe",
+            payment: false,
+            date: Date.now()
+        }
+        
+        const newOrder = new orderModel(orderData)
+        await newOrder.save();
+
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: cureency,
+                product_data: {
+                    name: item.name,
+                },
+                unit_amount: item.price*100
+            },
+            quantity: item.quantity
+
+        }))
+
+        line_items.push({
+             price_data: {
+                currency: cureency,
+                product_data: {
+                    name: deliveryCharge,
+                },
+                unit_amount: deliveryCharge*100
+            },
+            quantity: 1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/veryfy?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/veryfy?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: "payment",
+            
+        })
+
+        return res.status(201).json(
+            new ApiResponse(201, {url: session.url}, "Order placed successfully")   
+        );
+
 })
 
 // Placing order using Razorpay Payment Gateway
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import {ShopContext} from  "../context/ShopContext"
-import { assets } from "../assets/assets";
+import { assets } from "../assets/assets.js";
 import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 const Collection = () => {
